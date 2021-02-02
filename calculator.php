2cec2a83fb7f8ffeb281907aa908276ae66a528d
digital <max.lepan@laposte.net>
2021-02-02 16:19:04 +0100

    added unfinished calculator

---
+++ calculator.php
@@ -0,0 +1,14 @@
+<?
+
+print_r($argv);
+$number1 = $argv[1];
+$number2 = $argv[2];
+
+switch ($argv[0]) {
+	case + || "add":
+		echo $number1 + number2;
+		break;
+	case - || "sub":
+		echo number1 - number2;
+		break;
+}
